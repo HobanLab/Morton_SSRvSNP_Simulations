3a4428304ca2e37eb107c075c9dfc449e6da3ece
akoontz11 <austinkoontz11@gmail.com>
2024-01-09 14:22:42 -0600

    Added code for analyzing DNA low mutation datasets (N1200 and N4800)

--- RScripts/buildLinearModels.R
+++ RScripts/buildLinearModels.R
@@ -13,7 +13,8 @@
 # 2. One which sets the Total allelic representation as the response variable. These models are used 
 #    to examine how different simulation parameters impact estimates of allelic representation.
 
-# The script is split into 2 different sections based on total population size: N1200 and N4800
+# The script is split into 3 different sections based on total population size and mutation rate: N1200 and N4800
+# (MSAT and DNA; for DNA, mutation rate of 1e-7), and then low mutation rate DNA datasets (1e-8, N1200 and N4800)
 
 # Set working directory to the GitHub repo (containing scripts and fastSimcoal outputs)
 sim.wd <- '/home/akoontz/Shared/SSRvSNP_Sim/Code/'
@@ -91,7 +92,7 @@ boxplot(Total ~ nPops, data=N1200_MSAT_DF, ylab='Allelic representation (%)',
 plot(Total ~ nSamples, data=N1200_DNA_DF, ylab='Allelic representation (%)',
      xlab='Number of samples', pch=16, col=alpha("darkgreen", 0.10))
 # Title
-mtext("N1200: DNA: Allelic representation across scenario parameters", line=2, cex=1.5)
+mtext("N1200: SNP: Allelic representation across scenario parameters", line=2, cex=1.5)
 # Boxplots for migration rate and number of populations
 boxplot(Total ~ migRate, data=N1200_DNA_DF, ylab='Allelic representation (%)',
         xlab = 'Migration rate', pch=16)
@@ -164,9 +165,86 @@ boxplot(Total ~ nPops, data=N4800_MSAT_DF, ylab='Allelic representation (%)',
 plot(Total ~ nSamples, data=N4800_DNA_DF, ylab='Allelic representation (%)',
      xlab='Number of samples', pch=16, col=alpha("darkgreen", 0.10))
 # Title
-mtext("N4800: DNA: Allelic representation across scenario parameters", line=2, cex=1.5)
+mtext("N4800: SNP: Allelic representation across scenario parameters", line=2, cex=1.5)
 # Boxplots for migration rate and number of populations
 boxplot(Total ~ migRate, data=N4800_DNA_DF, ylab='Allelic representation (%)',
         xlab = 'Migration rate', pch=16)
 boxplot(Total ~ nPops, data=N4800_DNA_DF, ylab='Allelic representation (%)',
+        xlab = 'Number of populations', pch=16)
+
+# %%% DNA: LOW MUTATION RATE (1E-8) %%% ----
+# %%% N1200 ----
+# %%% Read in resampling arrays and build results data.frames
+N1200_DNA_lowMut_resampArr_filepath <- 
+  paste0(sim.wd, 'SimulationOutputs/DNA_N1200_lowMut/data.DNA/DNA_N1200_resampArr.Rdata')
+N1200_DNA_lowMut_resamplingArrays <-readRDS(N1200_DNA_lowMut_resampArr_filepath)
+# Transform resampling arrays into data.frames (for linear modeling)
+N1200_DNA_lowMut_DF <- rapply(N1200_DNA_lowMut_resamplingArrays, resample_array2dataframe, how = 'list')
+# Collapse multiple data.frames into single large data.frame (with all replicates of all scenarios;
+# this step takes multiple calls because we have a list of lists, and rbindlist cannot be rapply-ed)
+N1200_DNA_lowMut_DF <- lapply(N1200_DNA_lowMut_DF, rbindlist)
+N1200_DNA_lowMut_DF <- rbindlist(N1200_DNA_lowMut_DF)
+
+# %%% Predict 95% MSSEs
+# Declare a variable that's used as the prediction point (i.e. 95% "Total" allelic representation)
+predictPoint <- data.frame(Total=95)
+# Build model and predict
+N1200_DNA_lowMut_MSSEmodel <- lm(nSamples ~ Total, data=N1200_DNA_lowMut_DF)
+predict(N1200_DNA_lowMut_MSSEmodel, predictPoint, interval = "prediction")
+
+# %%% Build linear models to capture the impact of simulation parameters
+N1200_DNA_lowMut_model <- lm(Total ~ nSamples + nPops + migRate, data=N1200_DNA_lowMut_DF)
+summary(N1200_DNA_lowMut_model)
+summary.aov(N1200_DNA_lowMut_model)
+
+# %%% Plotting model results ----
+# Use layout to generate multiple plots in a single window
+layout(matrix(c(1,1,2,3), nrow=2, byrow=TRUE))
+# Standard resampling curve
+plot(Total ~ nSamples, data=N1200_DNA_lowMut_DF, ylab='Allelic representation (%)',
+     xlab='Number of samples', pch=16, col=alpha("darkblue", 0.10))
+# Title
+mtext("N1200: SNP: Low mutation (1e-8): Allelic representation across scenario parameters", line=2, cex=1.5)
+# Boxplots for migration rate and number of populations
+boxplot(Total ~ migRate, data=N1200_DNA_lowMut_DF, ylab='Allelic representation (%)',
+        xlab = 'Migration rate', pch=16)
+boxplot(Total ~ nPops, data=N1200_DNA_lowMut_DF, ylab='Allelic representation (%)',
+        xlab = 'Number of populations', pch=16)
+
+# %%% N4800 ----
+# %%% Read in resampling arrays and build results data.frames
+N4800_DNA_lowMut_resampArr_filepath <- 
+  paste0(sim.wd, 'SimulationOutputs/DNA_N4800_lowMut/data.DNA/DNA_N4800_resampArr.Rdata')
+N4800_DNA_lowMut_resamplingArrays <-readRDS(N4800_DNA_lowMut_resampArr_filepath)
+# Transform resampling arrays into data.frames (for linear modeling)
+N4800_DNA_lowMut_DF <- rapply(N4800_DNA_lowMut_resamplingArrays, resample_array2dataframe, how = 'list')
+# Collapse multiple data.frames into single large data.frame (with all replicates of all scenarios;
+# this step takes multiple calls because we have a list of lists, and rbindlist cannot be rapply-ed)
+N4800_DNA_lowMut_DF <- lapply(N4800_DNA_lowMut_DF, rbindlist)
+N4800_DNA_lowMut_DF <- rbindlist(N4800_DNA_lowMut_DF)
+
+# %%% Predict 95% MSSEs
+# Declare a variable that's used as the prediction point (i.e. 95% "Total" allelic representation)
+predictPoint <- data.frame(Total=95)
+# Build model and predict
+N4800_DNA_lowMut_MSSEmodel <- lm(nSamples ~ Total, data=N4800_DNA_lowMut_DF)
+predict(N4800_DNA_lowMut_MSSEmodel, predictPoint, interval = "prediction")
+
+# %%% Build linear models to capture the impact of simulation parameters
+N4800_DNA_lowMut_model <- lm(Total ~ nSamples + nPops + migRate, data=N4800_DNA_lowMut_DF)
+summary(N4800_DNA_lowMut_model)
+summary.aov(N4800_DNA_lowMut_model)
+
+# %%% Plotting model results ----
+# Use layout to generate multiple plots in a single window
+layout(matrix(c(1,1,2,3), nrow=2, byrow=TRUE))
+# Standard resampling curve
+plot(Total ~ nSamples, data=N4800_DNA_lowMut_DF, ylab='Allelic representation (%)',
+     xlab='Number of samples', pch=16, col=alpha("darkblue", 0.10))
+# Title
+mtext("N4800: SNP: Low mutation (1e-8): Allelic representation across scenario parameters", line=2, cex=1.5)
+# Boxplots for migration rate and number of populations
+boxplot(Total ~ migRate, data=N4800_DNA_lowMut_DF, ylab='Allelic representation (%)',
+        xlab = 'Migration rate', pch=16)
+boxplot(Total ~ nPops, data=N4800_DNA_lowMut_DF, ylab='Allelic representation (%)',
         xlab = 'Number of populations', pch=16)
